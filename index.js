e01bf0e1bfd3994b563ff04a6b4aa7d24a6618e0
Alex Vidal <arodriguez@tribalscale.com>
2018-06-05 13:57:15 +0400

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const port = 8080
+
+const server = http.createServer((req, res) => {
+	res.end('hello from server')
+})
+
+server.listen(port, () => {
+	console.log(`server running on $port`)
+})
